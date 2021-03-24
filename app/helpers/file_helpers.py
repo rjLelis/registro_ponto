9e6bd8a5250a6bec93d7433d23c292a2abce1811
Renato <renatojlelis@gmail.com>
2021-03-24 14:45:06 -0300

    Create file_helpers

---
+++ app/helpers/file_helpers.py
@@ -0,0 +1,27 @@
+import csv
+import datetime
+
+from models.dia_trabalho import DiaTrabalho
+
+
+def extract_dia_trabalho_from_csv(path: str) -> list[DiaTrabalho]:
+    if not path.endswith('.csv'):
+        path += '.csv'
+    registros = []
+    with open(path, 'r') as file:
+        leitor = csv.DictReader(file)
+        for registro in leitor:
+            data = datetime.datetime.strptime(registro['data'], '%d/%m/%y').date()
+            entrada = datetime.datetime.strptime(
+                    registro['entrada'], '%H:%M:%S').time() if registro['entrada'] else None
+            pausa = datetime.datetime.strptime(
+                    registro['pausa'], '%H:%M:%S').time() if registro['pausa'] else None
+            retorno = datetime.datetime.strptime(
+                    registro['retorno'], '%H:%M:%S').time() if registro['retorno'] else None
+            saida = datetime.datetime.strptime(
+                    registro['saida'], '%H:%M:%S').time() if registro['saida'] else None
+
+            registros.append(DiaTrabalho(
+                data, entrada, pausa, retorno, saida
+            ))
+    return registros
