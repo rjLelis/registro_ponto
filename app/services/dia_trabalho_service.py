62b0a1e1c6aeed34a91cef1786177c4447a7a643
Renato <renatojlelis@gmail.com>
2021-03-24 18:23:08 -0300

    Add services test

--- app/services/dia_trabalho_service.py
+++ app/services/dia_trabalho_service.py
@@ -2,17 +2,17 @@ import datetime
 import time
 import typing
 
-from models.dia_trabalho import DiaTrabalho, TipoResultado
+from app.models.dia_trabalho import DiaTrabalho, TipoResultado
 
 
 def calcular_horas(dia_trabalho: DiaTrabalho, _get_modelo_trabalho: typing.Callable) -> typing.Optional[tuple[typing.Union[None, str], TipoResultado]]:
 
     if dia_trabalho.data == datetime.date.today():
         return
-    elif any([horario is None for horario in [dia_trabalho.entrada, dia_trabalho.pausa, dia_trabalho.retorno, dia_trabalho.saida]]):
-        return (None, TipoResultado.REGISTRO_SEM_PAR)
     elif all(horario is None for horario in [dia_trabalho.entrada, dia_trabalho.pausa, dia_trabalho.retorno, dia_trabalho.saida]):
         return (None, TipoResultado.FALTA)
+    elif any([horario is None for horario in [dia_trabalho.entrada, dia_trabalho.pausa, dia_trabalho.retorno, dia_trabalho.saida]]):
+        return (None, TipoResultado.REGISTRO_SEM_PAR)
 
     entrada = datetime.datetime.combine(dia_trabalho.data, dia_trabalho.entrada)
     pausa = datetime.datetime.combine(dia_trabalho.data, dia_trabalho.pausa)
@@ -20,9 +20,11 @@ def calcular_horas(dia_trabalho: DiaTrabalho, _get_modelo_trabalho: typing.Calla
     saida = datetime.datetime.combine(dia_trabalho.data, dia_trabalho.saida)
 
     diff = ((saida - entrada) - (retorno - pausa))
+    print(diff.seconds)
 
     resultado = diff.seconds - _get_modelo_trabalho(dia_trabalho)
 
+    print(resultado)
     tipo_resultado = TipoResultado.handle_resultado(resultado)
 
     if resultado < 0:
